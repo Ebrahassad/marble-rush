06c31554b4a023b6d29a7ea791ec34d7521974a1
Ebrahassad <ebrahassadi1@gmail.com>
2026-09-16 17:46:08 +0200

    Add realistic asphalt ground texture and procedural starry night sky with fog

--- scripts/environment_theme.gd
+++ scripts/environment_theme.gd
@@ -2,11 +2,11 @@ extends WorldEnvironment
 
 func _ready() -> void:
 	var theme_data: Dictionary = ThemeManager.get_current_theme()
-	var sky_material := ProceduralSkyMaterial.new()
-	sky_material.sky_top_color = theme_data.sky_top
-	sky_material.sky_horizon_color = theme_data.sky_horizon
-	sky_material.ground_bottom_color = theme_data.sky_top
-	sky_material.ground_horizon_color = theme_data.sky_horizon
+	var sky_shader: Shader = load("res://shaders/sky.gdshader")
+	var sky_material := ShaderMaterial.new()
+	sky_material.shader = sky_shader
+	sky_material.set_shader_parameter("sky_top_color", theme_data.sky_top)
+	sky_material.set_shader_parameter("sky_horizon_color", theme_data.sky_horizon)
 
 	var sky := Sky.new()
 	sky.sky_material = sky_material
