eee729aa0a88b43874118dffecc179678dc38642
Ebrahassad <ebrahassadi1@gmail.com>
2026-09-19 23:37:27 +0200

    Add horse jumping obstacles model with textures and attribution credits

--- scripts/obstacle.gd
+++ scripts/obstacle.gd
@@ -1,15 +1,28 @@
 extends StaticBody3D
 
+var barrier_models: Array[String] = []
+
 
 func _ready() -> void:
 	add_to_group("obstacles")
-	_load_barrier_model()
+	_load_random_barrier()
 
 
-func _load_barrier_model() -> void:
-	var barrier_path := "res://assets/barriers/barrier.gltf"
-	if ResourceLoader.exists(barrier_path):
-		var barrier_scene := load(barrier_path) as PackedScene
+func _load_random_barrier() -> void:
+	var dir := DirAccess.open("res://assets/barriers/")
+	if dir:
+		dir.list_dir_begin()
+		var file_name := dir.get_next()
+		while file_name != "":
+			if not dir.current_is_dir():
+				var ext := file_name.get_extension().to_lower()
+				if ext in ["gltf", "glb", "tscn"]:
+					barrier_models.append("res://assets/barriers/" + file_name)
+			file_name = dir.get_next()
+
+	if barrier_models.size() > 0:
+		var random_path: String = barrier_models[randi() % barrier_models.size()]
+		var barrier_scene := load(random_path) as PackedScene
 		if barrier_scene:
 			var model := barrier_scene.instantiate()
 			add_child(model)
